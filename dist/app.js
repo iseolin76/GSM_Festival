11676ea59648ef4afbcdc2785c83687783665a55
seolin <iseolin76@gmail.com>
2020-12-10 14:57:07 +0900

    ADD connect Mongodb

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const node_config_ts_1 = require("node-config-ts");
+const mongoose = require("mongoose");
 class App {
     constructor() {
         this.application = express();
@@ -10,8 +12,10 @@ const app = new App().application;
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', './public/views');
+const connection = mongoose.connect(node_config_ts_1.config.MONGO_URI, { useNewUrlParser: true });
+console.log("connect mongodb");
 app.get("/", (req, res) => {
     res.render('index.html');
 });
-app.listen(4000, () => console.log(`Open Server http://127.0.0.1:4000`));
+app.listen(4000, () => console.log(`Open Server http://127.0.0.1:${node_config_ts_1.config.PORT}`));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import * as express from "express"
+import { config } from 'node-config-ts';
+import * as mongoose from 'mongoose'
+
 class App {
   public application: express.Application;
   constructor() {
     this.application = express();
   }
 }
+
 const app = new App().application;
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', './public/views');
 
+const connection = mongoose.connect(config.MONGO_URI, { useNewUrlParser: true });
+console.log("connect mongodb");
+
 app.get("/", (req: express.Request, res: express.Response) => {
   res.render('index.html');
 })
-app.listen(4000, () => console.log(`Open Server http://127.0.0.1:4000`)); 
+app.listen(4000, () => console.log(`Open Server http://127.0.0.1:${config.PORT}`)); 
