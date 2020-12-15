544b8a2b58911b6b27dd4cc60481a9c3f3ee2151
seolin <iseolin76@gmail.com>
2020-12-15 19:57:48 +0900

    Fix connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import * as express from "express"
+import * as express from 'express';
 import { config } from 'node-config-ts';
-import * as mongoose from 'mongoose'
+import * as mongoose from 'mongoose';
 
 class App {
-  public application: express.Application;
-  constructor() {
-    this.application = express();
-  }
+	public application: express.Application;
+	constructor() {
+		this.application = express();
+	}
 }
 
 const app = new App().application;
@@ -15,10 +15,15 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', './public/views');
 
-const connection = mongoose.connect(config.MONGO_URI, { useNewUrlParser: true });
-console.log("connect mongodb");
+const connection = mongoose.connect(config.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+console.log('connect mongodb');
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.render('index.html');
-})
-app.listen(4000, () => console.log(`Open Server http://127.0.0.1:${config.PORT}`)); 
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.render('index');
+});
+app.listen(8080, () =>
+	console.log(`Open Server http://127.0.0.1:${config.PORT}`)
+);
